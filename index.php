4618567c26fa6d14f9d96d953f41e39406bd1650
afrankhawksview <afrank@hawksviewhomes.com>
2015-09-15 13:36:15 -0400

    Added Slim Framework
    Still don't know what the hell I'm doing lets go yeah

--- index.php
+++ index.php
@@ -1,32 +1,15 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>To Do</title>
-	<link rel="stylesheet" href="styles/style.css">
-	<!-- Jquery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<?php
 
-	<!-- App -->
-	<script src="scripts/todo.js"></script>
-</head>
-<body>
-	<div class="app">
-		<h1>To Do</h1>
+require __DIR__ . '/vendor/autoload.php';
 
-		<form action="#">
-			<div class="list">
-			</div>
-			<div>
-				<label for="name">Text Input:</label>
-				<input type="text" name="thingToDo" id="thingToDo"/>
-			</div>
-			<div>
-				<input type="submit" value="Update" />
-			</div>
-		</form>
-	</div>
-</body>
-</html>
+date_default_timezone_set ('America/Toronto');
+
+
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+$app->get('/', function () {
+    include 'views/main.php';
+});
+$app->run();
---
+++ views/main.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>To Do</title>
+	<link rel="stylesheet" href="styles/style.css">
+	<!-- Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+	<!-- App -->
+	<script src="scripts/todo.js"></script>
+</head>
+<body>
+	<div class="app">
+		<h1>To Do</h1>
+
+		<form action="#">
+			<div class="list">
+			</div>
+			<div>
+				<label for="name">Text Input:</label>
+				<input type="text" name="thingToDo" id="thingToDo"/>
+			</div>
+			<div>
+				<input type="submit" value="Update" />
+			</div>
+		</form>
+	</div>
+</body>
+</html>
